62b13e9666c254210da8061e41c8652de07fc9a7
anosaryev <antnxv@gmail.com>
2025-03-02 21:34:54 -0500

    elaborated on function descriptions

--- scan.h
+++ scan.h
@@ -28,20 +28,24 @@ int print_ports(int *start, int *prev, int curr, int *print_start);
 // system/process errors
 int print_error(long *port_status, int err);
 
-// Iterates through the port range
-// specified and scans the supplied
-// IP address at each port.
+// Creates a non-blocking socket
+// and initiates a TCP connection.
+// Adds socket to event polling for
+// connection detection.
 // Returns 0 on immediate connection
 // or a pointer to the dynamically
 // allocated epoll data structure
 // for subsequent freeing.
-int scan_ports(char **dest, int *p_start, int *p_end);
-
-// Creates a non-blocking socket
-// and initiates a TCP connection.
-// Returns a pointer to the socket's
-// allocated event data or 0 if an
-// immediate connection is formed.
 long scan_port(char **dest, int p, int efd);
+
+// Iterates through the port range
+// specified and scans the supplied
+// IP address at each port with
+// nonblocking sockets in batches
+// of fixed size. Repeatedly waits
+// for connection results until
+// timeout, prints results via
+// print_ports().
+int scan_ports(char **dest, int *p_start, int *p_end);
 
 #endif // SCAN_H
